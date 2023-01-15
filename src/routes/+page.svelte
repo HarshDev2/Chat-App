30defed4b15ca1a264cbd2df8d0defc86278800d
Harsh Agrawal <averageworkspace@gmail.com>
2023-01-15 09:01:48 +0530

    Intial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script>
+
+    import Filter from 'bad-words';
+    import io from 'socket.io-client'
+    
+    let inputValue = '';
+    const filter = new Filter();
+    
+    // @ts-ignore
+    /**
+	 * @type {any[]}
+	 */
+    let messages = [];
+
+    function handleSubmit() {
+        const filter = new Filter();
+        console.log(inputValue)
+    }
+</script>
+
+<h1>Chat Room</h1>
+<div>
+
+    <scroller style="height: 400px; overflow: scroll;">
+        {#each messages as message}
+        <div>
+            <p><strong>{message}</p>
+        </div>
+        {/each}
+    </scroller>
+
+    <form on:submit|preventDefault={handleSubmit}>
+        <input type="text" bind:value={inputValue} />
+        <button type="submit">Send</button>
+    </form>
+
+</div>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+    import supabase from '@supabase/supabase-js'
+    
+    // Create a single supabase client for interacting with your database
+    const client = supabase.createClient('https://qpwqpaddpxmhqqqemrir.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwd3FwYWRkcHhtaHFxcWVtcmlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzM3MDM2MTYsImV4cCI6MTk4OTI3OTYxNn0.YKulK5MO3bsQUCEDK54Io49W6xVfZgrzio74Gr6UfS8')
+
+    let email = '';
+    let password = '';
+    let passwordagain = '';
+
+    function handleSignUp() {
+        client.auth.signUp({
+            email: email,
+            password: password,
+        })
+        alert(`Logining with email - ${email}, password - ${password}, passwordagain - ${passwordagain}`);
+    }
+    function handleLogin() {
+        alert(`Logining with email - ${email}, password - ${password}`);
+    }
+</script>
+
+<h1>SignUp</h1>
+<div>
+    
+    <form on:submit|preventDefault={handleSignUp}>
+        <input type="email" bind:value={email} placeholder="Email"/>
+        <input type="text" placeholder="Password" bind:value={password}/>
+        <input type="text" placeholder="Password Again" bind:value={passwordagain}/>
+        <button type="submit">SignUp</button>
+    </form>
+</div>
+
+<h1>Login</h1>
+<div>
+    <form on:submit|preventDefault={handleLogin}>
+        <input type="email" bind:value={email} placeholder="Email" />
+        <input type="text" bind:value={password} placeholder="Password">
+        <button type="submit">Login</button>
+    </form>
+</div>
